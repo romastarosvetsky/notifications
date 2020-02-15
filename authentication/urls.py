fcbe49fa206dcdad62388407d894b149a071fc1e
Starosvetsky, Roman <r.starosvetsky@itransition.com>
2020-02-15 12:45:51 +0300

    Create profile view.

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,5 @@ app_name = 'authentication'
 urlpatterns = [
     path('sign_up/', views.RegistrationAPIView.as_view()),
     path('log_in/', views.LogInAPIView.as_view()),
+    path('profile/', views.ProfileAPIView.as_view())
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from authentication.models import User
 from authentication.serializers import UserSerializer
 
 
@@ -31,3 +33,13 @@ class LogInAPIView(APIView):
                 token, _ = Token.objects.get_or_create(user=user)
                 return Response({'token': token.key})
         return Response({'details': 'Credentials are invalid.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileAPIView(RetrieveAPIView):
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, ]
+
+    def get_object(self):
+        return self.queryset.get(pk=self.request.user.pk)
